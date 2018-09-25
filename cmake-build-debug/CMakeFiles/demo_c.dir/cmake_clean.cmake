2790df4037a1c33e560219c05c86da6343e60b62
nguyenhakhoa <khoazxc511@gmail.com>
2018-09-24 19:32:05 -0700

    check

---
+++ cmake-build-debug/CMakeFiles/demo_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_c.dir/main.c.obj"
+  "demo_c.pdb"
+  "demo_c.exe"
+  "demo_c.exe.manifest"
+  "libdemo_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
